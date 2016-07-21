3fea8929d0d69fd5100e5a58fdb1c05e5ed23e05
Travis Rotz <tmrotz@gmail.com>
2016-07-21 00:26:53 -0500

    Make it work with popup.html

--- chrome-extension/LinkedInScraper/background.js
+++ chrome-extension/LinkedInScraper/background.js
@@ -1,17 +1,31 @@
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+function scrape() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {greeting: 'hello'}, function(response) {
       if (response.farewell == 'goodbye') {
 
-        chrome.tabs.create({url : 'https://104.236.247.194:3000/people/create'}, function (tab) {
+        chrome.tabs.create(
+          {
+            url : 'https://104.236.247.194:3000/people/create',
+            active: false
+          },
+            function (tab) {
+              
           var code = "var data = " + JSON.stringify(response.data) + ";";
           chrome.tabs.executeScript(tab.id, {code: code}, function() {
-            chrome.tabs.executeScript(tab.id, {file: 'dev.js'});
+            chrome.tabs.executeScript(tab.id, {file: 'dev.js'}, function() {
+              chrome.tabs.update(tab.id, {active: true});
+            });
           });
+          
         });
 
       }
     });
   });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#scrape').addEventListener(
+      'click', scrape);
 });
